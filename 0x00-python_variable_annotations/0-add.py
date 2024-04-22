a0a65338692b56b831e4576a2b97962b0bd012a3
gentleokhai <gentleokhaiX@gmail.com>
2024-04-22 23:27:30 +0100

    annot

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+The task 0's module.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Adds two floating-point numbers.
+    '''
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+The task 1's module.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Concatenates two strings.
+    '''
+    return str1 + str2
